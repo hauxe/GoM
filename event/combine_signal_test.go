cf5f8295fbc00956f752d2852178e379786ee735
hauma <hau@trustingsocial.com>
2018-07-24 10:52:31 +0700

    add combine and channels

--- event/combine_signal_test.go
+++ event/combine_signal_test.go
@@ -27,3 +27,24 @@ func TestOR(t *testing.T) {
 	)
 	require.WithinDuration(t, time.Now(), start, 2*time.Second)
 }
+
+func TestAND(t *testing.T) {
+	t.Parallel()
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-AND(
+		sig(200*time.Millisecond),
+		sig(500*time.Millisecond),
+		sig(1000*time.Millisecond),
+		sig(10*time.Millisecond),
+		sig(100*time.Millisecond),
+	)
+	require.WithinDuration(t, time.Now(), start, 1000*time.Millisecond+100*time.Millisecond)
+}
